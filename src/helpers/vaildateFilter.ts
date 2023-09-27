cc0f3617ef45d9c65f255bcd569689efc337e2fc
shirCitron <shirpb@gamil.com>
2023-09-27 16:08:47 +0300

    fix: correct typing

--- src/helpers/vaildateFilter.ts
+++ src/helpers/vaildateFilter.ts
@@ -15,15 +15,16 @@ type ValidOperatorsForFields = '<' | '>' | '<=' | '>=' | '=' | '!=' | 'st_contai
 interface FilterObject {
   field: ValidFields;
   operator: ValidOperatorsForFields;
-  filterArg: any;
+  value: string | number;
 }
 
 function isArrayOfObjectsValid(arr: FilterObject[]): boolean {
   const seenFields: Set<ValidFields> = new Set();
+
   for (const filterObj of arr) {
     let objectKeysValid: boolean = true;
-    Object.keys(filterObj).forEach((key: any): void => {
-      objectKeysValid = objectKeysValid && ['field', 'operator', 'filterArg'].includes(key);
+    Object.keys(filterObj).forEach((key: string): void => {
+      objectKeysValid = objectKeysValid && ['field', 'operator', 'value'].includes(key);
     });
     if (Object.keys(filterObj).length != 3 || !objectKeysValid) return false;
 
@@ -92,10 +93,11 @@ function isValidOperatorForField(field: ValidFields, operator: ValidOperatorsFor
 export default isArrayOfObjectsValid;
 // Example usage:
 const filters: FilterObject[] = [
-  { field: 'id', operator: '=', filterArg: 42 },
-  { field: 'name', operator: '=', filterArg: 'John' },
-  { field: 'description', operator: '=', filterArg: 'Some description' },
-  { field: 'bounding_polygon', operator: 'st_contains', filterArg: 'some polygon' },
+  { field: 'id', operator: '=', value: 42 },
+  { field: 'name', operator: '=', value: 'John' },
+  { field: 'description', operator: '=', value: 'Some description' },
+  { field: 'bounding_polygon', operator: 'st_contains', value: 'some polygon' },
 ];
+
 
 const isValid = isArrayOfObjectsValid(filters);
